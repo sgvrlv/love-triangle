87d2bfa2571ff07c94f2d3104e289ffdfdd56eef
sgvrlv <sasha.gavrilova.1998@yandex.by>
2019-03-12 17:03:16 +0300

    done

--- src/index.js
+++ src/index.js
@@ -1,7 +1,19 @@
-/**
+  /**
  * @param preferences - an array of integers. Indices of people, whom they love
  * @returns number of love triangles
  */
 module.exports = function getLoveTrianglesCount(preferences = []) {
-  // your implementation
+  let numberLove = 0, firstNum, secondNum, thirdNum;
+        
+  for (i = 0; i < preferences.length; i++) {
+    firstNum = preferences[i] - 1;
+    secondNum = preferences[firstNum] - 1;
+    thirdNum = preferences[secondNum] - 1;
+    if ((i == thirdNum) && (firstNum != secondNum) && (secondNum != thirdNum) && (thirdNum != firstNum)) {
+      numberLove++;
+    }
+  }
+  return numberLove / 3;
 };
+};
+
